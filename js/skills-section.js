9aa66e15913b2b37b84de9ff05b25dc218c60c4d
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-02-22 16:32:51 +0300

    Add "My Skills" section

---
+++ js/skills-section.js
@@ -0,0 +1,43 @@
+const skills = document.querySelectorAll(".skill");
+
+if (skills) {
+
+    const tooltipStyle = {
+        position: "fixed",
+        transition: ".3s",
+        padding: "5px",
+        animation: "tooltip-appearing .3s ease-in",
+        backgroundColor: "black",
+        textWrap: "nowrap",
+        zIndex: "1000",
+        borderRadius: "5px",
+        boxShadow: "0 0 7px -4px var(--primary-color)"
+    }
+    const tooltip = document.createElement("p");
+    tooltip.className = "tooltip";
+    Object.assign(tooltip.style, tooltipStyle);
+
+    skills.forEach((skill) => {
+        skill.addEventListener("mouseenter", () => {
+            if (!skill.querySelector(".tooltip")) {
+                const skillPosition = skill.getBoundingClientRect();
+                var x = skillPosition.x + skillPosition.width / 2;
+                var y = skillPosition.y + skillPosition.height / 2;
+
+                const skillDescriptio = skill.querySelector("img")?.getAttribute("alt");
+                if (skillDescriptio) {
+                    tooltip.textContent = skillDescriptio;
+                    skill.appendChild(tooltip);
+                    const tooltipPosition = tooltip.getBoundingClientRect();
+                    const tooltipWifth = tooltipPosition.width;
+                    const tooltipHeight = tooltipPosition.height;
+                    tooltip.style.top = `${y - tooltipHeight / 2}px`;
+                    tooltip.style.left = `${x - tooltipWifth / 2}px`;
+                }
+            }
+        })
+        skill.addEventListener("mouseleave", () => {
+            skill.removeChild(skill.querySelector(".tooltip"))
+        })
+    })
+}
