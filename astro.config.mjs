bde18df527309179ea599677afd0c2d48c2ef846
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-09-22 22:39:41 +0300

    Configure `pnpm`, `astro` and `tailwind`

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+    devToolbar: {
+        enabled: false
+    },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
